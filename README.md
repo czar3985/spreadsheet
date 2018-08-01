8991fa896fc7315b54cc237c69629fb523ddb8e0
Rina Lingad <pixie.czar@gmail.com>
2018-08-01 23:14:53 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Spreadsheet
+
+Online spreadsheet
+
+## To Do
+
+Coding ongoing on all features
---
+++ js/main.js
@@ -0,0 +1,5 @@
+/* Allow the column header and row number column to scroll accordingly */
+$(window).scroll(function () {
+    $('.column-header').css('left', (-window.pageXOffset).toString() + 'px');
+    $('.row-number-section').css('top', (202-window.pageYOffset).toString() + 'px');
+});
